8e8b1c50703357e63c8d140daeb5e9ade7cebf0d
coolguyjuls <129122097+coolguyjuls@users.noreply.github.com>
2025-07-21 12:47:44 -0500

    Create Logic For Weather Functions

--- task-weather-tracker/src/Weather.jsx
+++ task-weather-tracker/src/Weather.jsx
@@ -1,15 +1,59 @@
-
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Weather(){
+    const apiKey = "0af5588215a1eae42c5da0435aab14dc";
+
+    const [city, setCity] = useState("");
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(()=> {
+        const savedCity = localStorage.getItem('savedCity');
+        if (savedCity){
+            setCity(savedCity);
+            
+
+        }
+
+    })
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        // city ? getWeatherData(city) : displayError("please enter a city yo");
+        if (city){
+            try{
+                const apiURL= `https://api.weatherstack.com/current?access_key=${apiKey}&query=${city}&units=f`; 
+                const response = await fetch(apiURL);
+                const data = await response.json(); 
+
+                setWeatherData(data)
+                localStorage.setItem("savedCity", city) 
+            }
+            catch(error){
+                console.log(error);
+                displayError(error)
+            }
+        }else{
+            displayError("please enter a city yo");
+        }
+    };
+    
+
+    console.log(weatherData)
 
     return(
         <div className="weather-container">
-            <div className="weather-card">
-                <h3>City Name</h3>
-                <p>time</p>
-                <p>temp</p>
-                <p>weather Description</p>
-            </div>
+            <form className="weather-form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter City Here:" value={city} onChange={e => setCity(e.target.value)}></input>
+            </form>
+            {weatherData && 
+                <div className="weather-card">
+                    <h3>{weatherData.location.name}, {weatherData.location.region}</h3>
+                    <p>{weatherData.current.temperature}</p>
+                    <p>{weatherData.current.humidity}</p>
+                    <p>{weatherData.current.weather_descriptions[0]}</p>
+                </div>
+            }
         </div>
     );
 }
